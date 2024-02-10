a6685d960e1d50132dae7f192a100354dc7852a5
MohamedOsamaDev <mohamedosama10085@gmail.com>
2024-02-10 19:49:31 +0200

    new

--- src/modules/user/user.validation.js
+++ src/modules/user/user.validation.js
@@ -1,16 +1,32 @@
 import joi from "joi";
 
 const addUserVal = joi.object({
-  name: joi.string().pattern(/[a-zA-Z]/).min(2).max(20).required(),
+  name: joi
+    .string()
+    .pattern(/[a-zA-Z]/)
+    .min(2)
+    .max(20)
+    .required(),
   email: joi.string().email().required(),
-  password: joi.string().pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/).required(),
+  password: joi
+    .string()
+    .pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/)
+    .required(),
   confirmPassword: joi.valid(joi.ref("password")).required(),
-  age: joi.number().integer().min(10).max(80).required()
+  age: joi.number().integer().min(10).max(80).required(),
 });
 
 const updateAccVal = joi.object({
-  firstName: joi.string().pattern(/[a-zA-Z]/).min(2).max(20),
-  lastName: joi.string().pattern(/[a-zA-Z]/).min(2).max(20),
+  firstName: joi
+    .string()
+    .pattern(/[a-zA-Z]/)
+    .min(2)
+    .max(20),
+  lastName: joi
+    .string()
+    .pattern(/[a-zA-Z]/)
+    .min(2)
+    .max(20),
   email: joi.string().email(),
   recoveryEmail: joi.string().email(),
   DOB: joi.date(),
@@ -18,32 +34,33 @@ const updateAccVal = joi.object({
 });
 
 const otpVal = joi.object({
-  email: joi.string().email().required(),
-  newPassword: joi.string().pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/).required(),
+  newPassword: joi
+    .string()
+    .pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/)
+    .required(),
   confirmPassword: joi.valid(joi.ref("newPassword")).required(),
   otp: joi.string().required(),
-})
+});
 
 const forgetPasswordVal = joi.object({
   email: joi.string().email().required(),
 });
 
 const paramsIdVal = joi.object({
-   id : joi.string().length(24).hex().required()
+  id: joi.string().length(24).hex().required(),
 });
 
 const paramsEmailVal = joi.object({
-  recoveryEmail : joi.string().email().required()
+  recoveryEmail: joi.string().email().required(),
 });
 
 export {
-addUserVal, 
-paramsIdVal, 
-updateAccVal, 
-forgetPasswordVal,
-otpVal,
-paramsEmailVal
- };
-
+  addUserVal,
+  paramsIdVal,
+  updateAccVal,
+  forgetPasswordVal,
+  otpVal,
+  paramsEmailVal,
+};
 
 //.pattern(/^[a-z0-9A-Z]{2-50}@(gmail|yahoo).com$/)
